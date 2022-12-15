18fb136ca724b4236db5147af46f42d7a3ce4164
Ekaterina_P <zazie25@yandex.ru>
2022-12-15 21:46:08 +0300

    add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int square = number2 * number2;
+
+if (square == number1)
+Console.WriteLine ("yes");
+else Console.WriteLine("no");
