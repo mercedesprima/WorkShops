b035c131f181d5da9f1ff5bfc10ddb48c8a72901
Ekaterina_P <zazie25@yandex.ru>
2022-12-16 14:13:43 +0300

    Add Task 05

---
+++ Task05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count != number);
+{
+    Console.Write(count + " ");
+    count++;
+}
