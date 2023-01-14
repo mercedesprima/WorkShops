3ec965358ea2ec72f5ae65790fe061d5f8d04fde
Ekaterina_P <zazie25@yandex.ru>
2023-01-14 21:09:39 +0300

    Task 24

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int countNumbers = CountNumbers(number);
+Console.Write($"Количество цифр в числе = {countNumbers}");
+
+int CountNumbers(int digit)
+{
+    int count = 0;
+    
+    while (digit != 0)
+    {
+       digit = digit / 10;
+       count++;
+    }
+    return count;
+}
---
+++ Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// if number <= 0
+
+int factorialNumber = FactorialNumber(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorialNumber}");
+
+
+int FactorialNumber(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            result *= i;
+        }
+    }
+    return result;
+}
