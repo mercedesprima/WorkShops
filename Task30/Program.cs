1d7dc7e3b21f6482c91032aa9478183331532df9
Ekaterina_P <zazie25@yandex.ru>
2023-01-16 23:00:20 +0300

    add Task 30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -25,22 +25,22 @@
 // }
 int[] array = new int[8]; // 0,1,2,3,4,5,6,7
 
-            FillArray(array);
-            PrintArray(array);
+FillArray(array);
+PrintArray(array);
 
-            void FillArray(int[] arr)
-            {
-                Random rnd = new Random();
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    arr[i] = rnd.Next(0, 2);
-                }
-            }
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
 
-            void PrintArray(int[] arr)
-            {
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    Console.Write(arr[i] + " ");
-                }
-            }
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
