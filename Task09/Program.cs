ef90ec319e54f91cb9894fae596b4652af3bca4d
Ekaterina_P <zazie25@yandex.ru>
2023-01-18 16:26:38 +0300

    add Task 9

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -7,9 +7,6 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
 
-// int firstDigit = number / 10; 
-// int secondDigit = number % 10;
-
 // if (firstDigit > secondDigit) 
 //  {
 //     Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
@@ -22,7 +19,6 @@ Console.WriteLine($"Случайное число из отрезка 10 - 99 =>
 
 // int result = firstDigit > secondDigit ? firstDigit : secondDigit;
 // Console.WriteLine($"Наибольшая цифра числа => {result}");
-
 
 int maxDigit = MaxDigit(number);
 Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
