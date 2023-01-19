4711dfe556678fdba4488718a7899c4ad3c202d3
Ekaterina_P <zazie25@yandex.ru>
2023-01-19 21:28:15 +0300

    add Task 42

---
+++ Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string res = ConvertDecToBin(13);
+Console.WriteLine(res);
+
+string ConvertDecToBin(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
