7d3be19dc731f613203724465d9017ef59478d6d
Ekaterina_P <zazie25@yandex.ru>
2023-01-20 17:40:16 +0300

    add Task 45

---
+++ Task45/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+//     int[] arr = new int[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rnd.Next(min, max + 1);
+//     }
+//     return arr;
+// }
+
+// void PrintArray(int[] arr)
+
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+//         else Console.Write(arr[i]);
+//     }
+//     Console.WriteLine("]");
+// }
+
+// int CopyArray(int[] arr)
+// {
+//     int[] array = new int[arr.Length];
+//     for (int i = 0; i < arr.length; i++)
+//     {
+//         array[i] = arr[];
+//     }
+//     return array;
+// }
+
+int[] arrayOrigin = CreateArrayRndInt(10, -3, 5);
+int[] arrayCopy = CopyArray(arrayOrigin);
+//arrayCopy[0] = 30;
+PrintArray(arrayOrigin);
+PrintArray(arrayCopy);
+
+
+int[] CopyArray(int[] arr)
+{
+    int[] result = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result[i] = arr[i];
+    }
+    return result;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] newArray = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = rnd.Next(min, max + 1);
+    }
+    return newArray;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+
+}
