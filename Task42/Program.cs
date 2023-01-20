b2623a3d3c826ec33ef43ab0381d96c25f5516b0
Ekaterina_P <zazie25@yandex.ru>
2023-01-20 17:44:07 +0300

    add Task 42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,10 +1,11 @@
-﻿// // Задача 42: Напишите программу, которая будет преобразовывать
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
 // десятичное число в двоичное.
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
 
-string res = ConvertDecToBin(13);
+                                        // Вариант 1
+string res = ConvertDecToBin(2);
 Console.WriteLine(res);
 
 string ConvertDecToBin(int num)
@@ -15,5 +16,27 @@ string ConvertDecToBin(int num)
         result = num % 2 + result;
         num = num / 2;
     }
+    return result;
+}
+                                             // Вариант 2
+Console.Write("Введите число для преобразования в двоичную систему счисления: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int convertDecimalToBinary = ConvertDecimalToBinary(number);
+Console.WriteLine(convertDecimalToBinary);
+
+int ConvertDecimalToBinary(int num)
+{
+    int result = 0;
+    int multiplyer = 1;
+
+    while (num != 0)
+    {
+        result = result + num % 2 * multiplyer;
+        num = num / 2;
+        multiplyer *= 10;
+
+    }
+
     return result;
 }
