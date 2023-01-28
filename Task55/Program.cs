d36f1ed7d5312b50ae103510308a35ce2e38f2bd
Ekaterina_P <zazie25@yandex.ru>
2023-01-28 13:17:22 +0300

    add Task 55

---
+++ Task55/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+
+int[,] array2D = CreateMatrixRndInt(4, 4, 1, 11);
+PrintMatrix(array2D);
+
+if (array2D.GetLength(0) == array2D.GetLength(1))
+{
+    Console.WriteLine();
+    ChangeRowColumnsMatrix(array2D);
+    // int[,] newMatrix = ChangeRowColumnsMatrix(array2D);
+    // PrintMatrix(newMatrix);
+    PrintMatrix(array2D);
+}
+else Console.WriteLine("Поменять местами строки со столбцами невозможно");
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void ChangeRowColumnsMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0)-1; i++)
+    {
+        for (int j = i+1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+// int[,] ChangeRowColumnsMatrix(int[,] matrix)
+// {
+//     int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             newMatrix[i, j] = matrix[j, i];
+//         }
+//     }
+//     return newMatrix;
+// }
